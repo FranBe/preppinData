27b1afdadbcd9f9af0810f538eb5b6c5c10d2934
FrancoBe <{ID}+{username}@users.noreply.github.com>
2023-02-14 15:09:11 -0300

    first commit

---
+++ 2023/week1.R
@@ -0,0 +1,41 @@
+######################
+##  REQUIREMENTS    ##
+######################
+
+# 1) Input the data
+
+data <- read.csv('data/Week1_input.csv')
+
+# Inspection
+
+summary(data)
+
+# 2)  Split the Transaction Code to extract the letters at the start of the transaction code. 
+# These identify the bank who processes the transaction 
+
+
+
+
+# 3) Rename the new field with the Bank code 'Bank'. 
+
+
+# 4) Rename the values in the Online or In-person field.
+# Online of the 1 values and In-Person for the 2 values. 
+
+
+# 5) Change the date to be the day of the week
+
+
+# 6) Different levels of detail are required in the outputs. 
+# You will need to sum up the values of the transactions in three ways
+
+#  1. Total Values of Transactions by each bank
+#  2. Total Values by Bank, Day of the Week and Type of Transaction (Online or In-Person)
+#  3. Total Values by Bank and Customer Code
+  
+  
+  
+# 7) Output each data file (help)
+
+
+
